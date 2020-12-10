c5288d92e768876e51fc59df3588bbff9d67a4ea
fidoriel <49869342+fidoriel@users.noreply.github.com>
2020-12-10 15:35:44 +0100

    example changed, license and requirement added

--- example.py
+++ example.py
@@ -1,22 +1,28 @@
 import pyMCP23017
 from time import sleep
 
+# constructor
 mcp = pyMCP23017.MCP23017(0x20)
 
+# setup
 pin=15
 mcp.setup(pin, mcp.OUT)
+# turn pin on
 mcp.output(pin, mcp.HIGH)
 sleep(1)
 pin2=5
 mcp.setup(pin2, mcp.OUT)
+# turn pin on
 mcp.output(pin2, mcp.HIGH)
 sleep(1)
+# turn pin off
 mcp.output(pin2, mcp.LOW)
 mcp.output(pin, mcp.LOW)
 
 pin3 = 0
 mcp.setup(pin3, mcp.IN, pull_up_down = mcp.PUD_UP)
 
+# read pin3
 if mcp.input(pin3):
     print('True')
 else:
